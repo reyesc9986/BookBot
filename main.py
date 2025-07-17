d4221c30de5fc41d200927bec8603e1e172be5d9
reyesc9986 <reyesc9986@gmail.com>
2025-07-16 20:58:37 -0400

    final push before merging into main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import *
 def get_book_text(pathtobook):
     with open(pathtobook) as text:
@@ -5,17 +6,21 @@ def get_book_text(pathtobook):
         return contents
 
 def main():
-    booktext = get_book_text("books/frankenstein.txt")  
-    wordcount = wordcounter(booktext)
-    char_counter = get_character_count(booktext)
-    sorted_char_counter = sort_character_count(char_counter)
-    print("============ BOOKBOT ============")
-    print('Analyzing book found at books/frankenstein.txt...')
-    print("----------- Word Count ----------")
-    print(f'Found {wordcount} total words')
-    print('--------- Character Count -------')
-    for char_dict in sorted_char_counter:
-        print(str(char_dict["char"]) +": " + str(char_dict["num"]))
-    print("============= END ===============")
+    if len(sys.argv) == 2:
+        booktext = get_book_text(sys.argv[1])  
+        wordcount = wordcounter(booktext)
+        char_counter = get_character_count(booktext)
+        sorted_char_counter = sort_character_count(char_counter)
+        print("============ BOOKBOT ============")
+        print('Analyzing book found at books/frankenstein.txt...')
+        print("----------- Word Count ----------")
+        print(f'Found {wordcount} total words')
+        print('--------- Character Count -------')
+        for char_dict in sorted_char_counter:
+            print(str(char_dict["char"]) +": " + str(char_dict["num"]))
+        print("============= END ===============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 main()
 
